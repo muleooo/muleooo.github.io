527b5d1be519a1075df862521f17a3a9b261ca66
muleooo <seounghoun@gmail.com>
2020-06-29 10:59:17 +0200

    blah

---
+++ lookup.js
@@ -0,0 +1,15 @@
+function systemNametosystemID(systemName) {
+	systemID=0;
+	return systemID;}
+
+function systemIDtoSystemName(systemID) {
+	systemName=0;
+	return systemName;}
+
+function lookupSecurity(systemID) {
+	security=0;
+	return security;}
+	
+function lookupPVP(systemID) {
+	PVP=0;
+	return PVP;}
